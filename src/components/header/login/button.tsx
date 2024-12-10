9c3568bd97d649ca720e4cecdc6c4d7f9794453f
Vitor Thiz <77014702+thiiz@users.noreply.github.com>
2024-12-10 00:55:51 +0000

    shadcn

--- src/components/header/login/button.tsx
+++ src/components/header/login/button.tsx
@@ -4,15 +4,37 @@ import { LoginModal } from "@/components/login-modal"
 import Image from "next/image"
 import { useState } from "react"
 import { AnimatePresence } from 'framer-motion';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 export const LoginButton = () => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
     <>
       <button onClick={() => setIsOpen(prev => !prev)}>
-        <div className="relative w-10 h-10 rounded-full">
-          <Image fill className="rounded-full" alt="A" src="https://randomuser.me/api/portraits/women/68.jpg" />
-        </div>
+      <DropdownMenu>
+  <DropdownMenuTrigger><Avatar>
+          <AvatarImage src="https://github.com/shadcn.png"/>
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        </DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+    <DropdownMenuItem>Team</DropdownMenuItem>
+    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
       </button>
       <AnimatePresence mode="wait">
         {isOpen ? <LoginModal setState={setIsOpen} /> : null}
